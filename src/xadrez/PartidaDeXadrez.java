77ff1f13b93224eab5dd346b8808d3ccb174e724
Felipe Roque <feliperamosroque@gmail.com>
2020-04-12 22:16:02 -0300

    Aplicando cores para as peças

--- src/xadrez/PartidaDeXadrez.java
+++ src/xadrez/PartidaDeXadrez.java
@@ -30,9 +30,19 @@ public class PartidaDeXadrez {
 	}
 	
 	private void setupInicial() {
-		lugarNovaPeca('b', 6, new Torre(tabuleiro, Cor.BRANCO));
-		lugarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
-		lugarNovaPeca('e', 1, new Rei(tabuleiro, Cor.PRETO));
+		lugarNovaPeca('c', 1, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('c', 2, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('e', 2, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('e', 1, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('d', 1, new Rei(tabuleiro, Cor.BRANCO));
+
+        lugarNovaPeca('c', 7, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('c', 8, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('d', 7, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('e', 7, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('e', 8, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('d', 8, new Rei(tabuleiro, Cor.PRETO));
 	}
 
 }
